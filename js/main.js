04a3ddfe5561a7ca64b62c4e2486f0eaeab9cf7f
Юлия Стрилец <strilets@meiji.media>
2021-06-14 18:56:09 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@ function getRandomNumberInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 function getRandomNumberFloat(min, max, toFixed = 2) {
   if (min < 0 || max < 0) {
     return 'Вы ввели отрицательное число';
   }
   return ((Math.random() * (max - min + 1)) + min).toFixed(toFixed);
-}
+};
 
 getRandomNumberInt(0, 0);
 getRandomNumberFloat(3,156);
